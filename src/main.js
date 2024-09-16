1391035df4073cf5c56caad4ed3085d7883a40e7
SubatYucel <subat.yucel1@gmail.com>
2024-09-16 13:07:51 +0300

    axios  added loadmore btn etc

--- src/main.js
+++ src/main.js
@@ -2,27 +2,39 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
 const input = document.querySelector('.search-input');
 const searchBtn = document.querySelector('.search-btn');
 const gallery = document.getElementById('gallery');
-
 const lightbox = new SimpleLightbox('.gallery li > a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+const loadMoreBtn = document.getElementById('load-more');
+
+let page = 1;
+let curentQ = '';
 
 async function searchImages() {
-  const loader = document.getElementById('loader');
-  const q = input.value;
+  const q = input.value.trim();
+  let loader = document.getElementById('loader-more-container');
+
+  if (q !== '' && q !== curentQ) {
+    page = 1;
+    curentQ = q;
+    gallery.innerHTML = '';
+    loader = document.getElementById('loader-container');
+  }
+
   const apiKey = '45978686-70839b27c443bdf6e9ef42e3a';
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
   try {
-    gallery.innerHTML = '';
     loader.classList.remove('hidden');
-    const response = await fetch(url);
-    const data = await response.json();
+
+    const response = await axios.get(url);
+    const data = response.data;
 
     if (data.hits.length === 0) {
       iziToast.error({
@@ -33,6 +45,8 @@ async function searchImages() {
       });
     } else {
       displayImages(data.hits);
+      loadMoreBtn.classList.remove('hidden');
+      page++;
     }
   } catch (error) {
     console.error('Hata:', error);
@@ -59,12 +73,17 @@ function displayImages(images) {
     `;
   });
 
-  gallery.innerHTML = galleryHtml.join('');
+  gallery.innerHTML += galleryHtml.join('');
   lightbox.refresh();
 }
 
 searchBtn.addEventListener('click', e => {
   e.preventDefault();
 
+  searchImages();
+});
+
+loadMoreBtn.addEventListener('click', e => {
+  e.preventDefault();
   searchImages();
 });
